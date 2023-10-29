0aa1efcb7de38cfb66c1873ebc913c2e031a0db3
EldarHac <eldar.hacohen@gmail.com>
2023-10-29 11:48:01 +0200

    Python package to chat with scanned PDF in Hebrew / English

---
+++ pdf_ama.py
@@ -0,0 +1,153 @@
+# coding part
+import shutil
+
+import streamlit as st
+from deep_translator import GoogleTranslator
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import PyPDFLoader
+from langchain.schema import HumanMessage, AIMessage, SystemMessage
+from langchain.vectorstores.chroma import Chroma
+from streamlit_chat import message
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.callbacks import get_openai_callback
+
+import pickle
+import os
+
+# load api key lib
+from dotenv import load_dotenv
+
+from config import config
+from pdf_translator import translate_pdf
+
+# add_bg_from_local('images.jpeg')
+
+# sidebar contents
+
+with st.sidebar:
+    st.title("""🦜️ Multilingual ChatGPT for your scanned documents 🤗""")
+    st.markdown('''
+    ## About APP:
+
+    The app's primary resource is utilised to create::
+
+    - [streamlit](https://streamlit.io/)
+    - [Langchain](https://docs.langchain.com/docs/)
+    - [OpenAI](https://openai.com/)
+
+    ## About me:
+
+    - [Linkedin](https://www.linkedin.com/in/eldar-refael-hacohen-58b4b018a/)
+
+    ''')
+
+load_dotenv()
+
+
+def init():
+    # test that the API key exists
+    os.environ['OPENAI_API_KEY'] = config.get('OPENAI_API_KEY')
+    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
+        print("OPENAI_API_KEY is not set")
+        exit(1)
+    else:
+        print("OPENAI_API_KEY is set")
+
+    # # setup streamlit page
+    # st.set_page_config(
+    #     page_title="Your own ChatGPT",
+    #     page_icon="🤖"
+    # )
+
+
+def main():
+    init()
+
+    doc_lang = st.selectbox(
+        'Which language is your document in?',
+        ('Hebrew', 'English'))
+
+    st.write('You selected:', doc_lang)
+
+    tr = lambda msg: GoogleTranslator(source='auto', target='en').translate(msg)
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+
+    # upload a your pdf file
+    pdf = st.file_uploader("Upload your PDF", type='pdf')
+
+    if pdf is not None:
+        store_name = pdf.name[:-4]
+        DIR_NAME = f"{store_name}_dir"
+
+        # initialize message history
+        if "messages" not in st.session_state:
+            st.session_state.messages = [
+                SystemMessage(content="You are a helpful assistant."
+                                      "Please provide page numbers and part of the document"
+                                      "of where the answer is taken from.")
+            ]
+            os.makedirs(DIR_NAME, exist_ok=True)
+            with open(os.path.join(DIR_NAME, f"{store_name}_history.pkl"), "wb") as f:
+                pickle.dump([], f)
+
+        st.header("📄Chat with your pdf file🤗")
+
+        if os.path.exists(os.path.join(DIR_NAME, f"{store_name}.pkl")):
+            with open(os.path.join(DIR_NAME, f"{store_name}.pkl"), "rb") as f:
+                documents = pickle.load(f)
+            st.write("Already, Embeddings loaded from the your folder (disks)")
+        else:
+            if not os.path.exists(os.path.join(DIR_NAME, f"{store_name}.pkl")):
+                pdf_path = translate_pdf(pdf.name, DIR_NAME, translate=(doc_lang == "Hebrew"))
+                loader = PyPDFLoader(pdf_path)
+                documents = loader.load()
+                text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+                documents = text_splitter.split_documents(documents)
+
+                with open(os.path.join(DIR_NAME, f"{store_name}.pkl"), "wb") as f:
+                    pickle.dump(documents, f)
+
+        embeddings = OpenAIEmbeddings()
+        vectorstore = Chroma.from_documents(documents, embeddings)
+        text = st.text_input("Your message: ", key="user_input")
+        if len(text):
+            user_input = GoogleTranslator(source='auto', target='en').translate(text)
+            qa = ConversationalRetrievalChain.from_llm(llm, vectorstore.as_retriever())
+
+            if os.path.exists(os.path.join(DIR_NAME, f"{store_name}_history.pkl")):
+                with open(os.path.join(DIR_NAME, f"{store_name}_history.pkl"), "rb") as f:
+                    chat_history = pickle.load(f)
+                    print(chat_history)
+                st.write("Already, Embeddings loaded from the your folder (disks)")
+            else:
+                chat_history = []
+
+            # handle user input
+            if user_input:
+                st.session_state.messages.append(HumanMessage(content=text))
+                # docs = vectorstore.similarity_search(query=user_input, k=3)
+                with st.spinner("Thinking..."):
+                    with get_openai_callback() as cb:
+                        resp = qa({'question': user_input, "chat_history": chat_history})['answer']
+                        response = GoogleTranslator(source='auto', target='iw').translate(resp)
+                        print(cb)
+                st.session_state.messages.append(
+                    AIMessage(content=response))
+                chat_history.append((tr(text), resp))
+
+                with open(os.path.join(DIR_NAME, f"{store_name}_history.pkl"), "wb") as f:
+                    pickle.dump(chat_history, f)
+
+        # display message history
+        messages = st.session_state.get('messages', [])
+        for i, msg in enumerate(messages[1:]):
+            if i % 2 == 0:
+                message(msg.content, is_user=True, key=str(i) + '_user')
+            else:
+                message(msg.content, is_user=False, key=str(i) + '_ai')
+
+
+if __name__ == "__main__":
+    main()
---
+++ pdf_translator.py
@@ -0,0 +1,86 @@
+import os
+
+import cv2
+from pdf2image import convert_from_path
+import pytesseract
+from deep_translator import GoogleTranslator
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph, PageBreak
+from reportlab.lib.styles import getSampleStyleSheet
+from concurrent.futures import ThreadPoolExecutor
+
+
+# Sub-task 2: Convert each PDF page to an image
+def convert_pdf_to_images(input_pdf_path):
+    images = []
+    pages = convert_from_path(input_pdf_path, poppler_path='/opt/homebrew/Cellar/poppler/23.10.0/bin', thread_count=10)
+    images.extend(pages)
+    return images
+
+
+# Sub-task 3: Use OCR to extract Hebrew text from each image
+def process_image(i, image, dir_name):
+    pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/Cellar/tesseract/5.3.3/bin/tesseract'  # Update this path
+    IMAGE_PATH = os.path.join(dir_name, f'temp{i}.png')
+    image.save(IMAGE_PATH, 'png')
+    image = cv2.imread(IMAGE_PATH)
+    (h, w) = image.shape[:2]
+    img = cv2.resize(image, (w * 3, h * 3))
+    gry = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    thr = cv2.threshold(gry, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    text = pytesseract.image_to_string(thr, lang='heb')
+    text = text.replace('מייר', 'מ״ר')
+    text = text.replace('עייי', 'ע״י')
+    text = text.replace('ימיס', 'ימים')
+    os.remove(IMAGE_PATH)
+    return text
+
+
+def extract_text(images, dir_name):
+    pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/Cellar/tesseract/5.3.3/bin/tesseract'  # Update this path
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        texts = list(executor.map(lambda args: process_image(*args, dir_name), enumerate(images)))
+    return texts
+
+
+# Sub-task 4: Translate Hebrew text to English
+def translate_text(i, text):
+    return GoogleTranslator(source='auto', target='en').translate(text)
+
+
+def translate_texts(texts):
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        translated_texts = list(executor.map(lambda args: translate_text(*args), enumerate(texts)))
+    return translated_texts
+
+
+# Sub-task 5: Create a new PDF with the translated text, page by page
+def create_translated_pdf(translated_texts, input_pdf_path):
+    output_pdf_path = f'{input_pdf_path[:-4]}_translated.pdf'
+    doc = SimpleDocTemplate(output_pdf_path, pagesize=letter)
+    story = []
+    styles = getSampleStyleSheet()
+    style = styles["Normal"]
+    style.fontSize = 8
+    style.leading = 14
+    for translated_text in translated_texts:
+        pars = translated_text.split("\n\n")
+        for par in pars:
+            if not par.isdigit():
+                paragraph = Paragraph(par, style)
+                story.append(paragraph)
+        story.append(PageBreak())
+    doc.build(story)
+    return output_pdf_path
+
+
+def translate_pdf(input_pdf_path, dir_name, translate=True):
+    images = convert_pdf_to_images(input_pdf_path)
+    texts = extract_text(images, dir_name)
+    if translate:
+        texts = translate_texts(texts)
+    return create_translated_pdf(texts, input_pdf_path)
+
+
+if __name__ == '__main__':
+    translate_pdf('nushnush.pdf', 'nushnush_dir')
