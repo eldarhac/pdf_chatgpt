e9580e40e9f17fa741f9d04bc3757bcb1e1229d2
eldarhac <39159569+eldarhac@users.noreply.github.com>
2023-12-11 14:26:41 +0200

    Update pdf_ama.py

--- pdf_ama.py
+++ pdf_ama.py
@@ -71,7 +71,6 @@ def main():
         ('Hebrew', 'English'))
 
     st.write('You selected:', doc_lang)
-    st.write(os.listdir(os.path.join(os.getcwd(), '../../../usr/bin/poppler/')))
     tr = lambda msg: GoogleTranslator(source='auto', target='en').translate(msg)
     llm = ChatOpenAI(model_name="gpt-3.5-turbo")
 
