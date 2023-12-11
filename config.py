0f21c1b40c9697e5e86621209c904d7d6089035f
eldarhac <39159569+eldarhac@users.noreply.github.com>
2023-12-11 14:04:49 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 config = {"OPENAI_API_KEY": "sk-",  # Insert your OpenAI API key
-          "POPPLER_PATH": r'/usr/bin/poppler/20.09.0/bin',  # Update your Poppler path
+          "POPPLER_PATH": r'/usr/bin/poppler/bin',  # Update your Poppler path
           "TESSERACT_PATH": r'/usr/bin/tesseract/5.3.3/bin/tesseract'}  # Update your Tesseract path
