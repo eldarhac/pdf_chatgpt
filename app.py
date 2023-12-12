e30b15e8881a4aac4374abf641f24a5c80151b91
eldarhac <39159569+eldarhac@users.noreply.github.com>
2023-12-12 16:42:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, send_file
 from pdf_ama import translate_pdf
 import os
+import tempfile
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
@@ -14,13 +15,20 @@ def upload_file():
         if file.filename == '':
             return render_template('upload.html', message='No selected file')
         if file:
-            filename = file.filename
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)
+                        # Save the uploaded file to a temporary file
+            temp_dir = tempfile.TemporaryDirectory()
+            temp_path = os.path.join(temp_dir.name, file.filename)
+            file.save(temp_path)
 
-            translated_file = translate_pdf(file_path)
+            translated_file_path = translate_pdf(temp_path)
 
-            return send_file(translated_file, as_attachment=True)
+            # Send the translated file to the user
+            return_data = send_file(translated_file_path, as_attachment=True)
+
+            # Clean up the temporary directory
+            temp_dir.cleanup()
+
+            return return_data
     return render_template('upload.html', message=None)
 
 if __name__ == '__main__':
