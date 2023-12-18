45119f2651da0c12151500caae0aec968b93b552
eldarhac <39159569+eldarhac@users.noreply.github.com>
2023-12-18 13:58:21 +0200

    Update pdf_translator.py

--- pdf_translator.py
+++ pdf_translator.py
@@ -96,7 +96,7 @@ def translate_pdf(input_pdf_path, translate=True):
     for i, image in enumerate(images):
         text_job = q.enqueue(process_image, i, image, dir_name)
         if translate:
-            text_job = q.enqueue(translate_text, i, text, depends_on[text_job])
+            text_job = q.enqueue(translate_text, i, text_job.return_value(), depends_on[text_job])
         texts.append(text.return_value())
     translated_pdf = create_translated_pdf(texts, input_pdf_path)
     return translated_pdf
