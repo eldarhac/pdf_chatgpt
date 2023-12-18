f7b12a54d493aa7479c52e3a15e0fd4d004e1025
eldarhac <39159569+eldarhac@users.noreply.github.com>
2023-12-18 20:54:17 +0200

    Update worker.py

--- worker.py
+++ worker.py
@@ -5,7 +5,7 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDIS_URL', 'rediss://:p5a25225b6d26da7c5a06b52c7e1502313124197be3d337fe9c125120c9866acb@ec2-54-211-50-118.compute-1.amazonaws.com:6400')
+redis_url = os.getenv('REDIS_URL', 'rediss://:p661f2835f08b5a51b4c6f9fd13caa598272d3e9c83ab60438531929cee2753e4@ec2-44-214-199-53.compute-1.amazonaws.com:24419')
 
 conn = redis.from_url(redis_url)
 
