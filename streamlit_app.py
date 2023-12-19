fb53a67c3791ba167de8f850a220d8d0d4b06f54
eldarhac <39159569+eldarhac@users.noreply.github.com>
2023-12-19 12:04:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,10 @@ import tempfile
 from pdf_translator import translate_pdf
 from random import randint
 
+holder = st.empty()
+
 # File uploader allows the user to upload a PDF
-uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+uploaded_file = holder.file_uploader("Choose a PDF file", type="pdf", key=2)
 
 if uploaded_file is not None:
     # Save the file to a temporary directory
@@ -14,13 +16,12 @@ if uploaded_file is not None:
 
     # Provide a link to download the translated file
     with open(translated_file_path, "rb") as file:
-        if st.download_button(
+        btn = st.download_button(
             label="Download Translated PDF",
             data=file,
             file_name=f"{uploaded_file.name[:-4]}_translated.pdf",
             mime="application/octet-stream"
-        ):
-            #Optional: Clean up if you want to delete the temporary files
-            os.remove(translated_file_path)
-            uploaded_file.key = str(randint(1000, 100000000))
-            st.rerun()
+        )
+    #Optional: Clean up if you want to delete the temporary files
+    os.remove(translated_file_path)
+    holder.empty()
